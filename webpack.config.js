d61f6499d19112c431da4f8aa83f5cd91d0dfb4e
Nat Okpe <natokpe@gmail.com>
2024-04-25 22:03:31 +0100

    Configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path    = require( 'path' );
+const webpack = require( 'webpack' );
+
+module.exports = ( env, options ) => {
+    return {
+        mode    : 'production',
+        watch   : true,
+        devtool : false,
+
+        entry   : {
+            script: './assets/js/index.js'
+        },
+
+        output: {
+            path    : path.join(__dirname),
+            filename: '[name].js'
+        },
+
+        module  : {
+        },
+
+        resolve: {
+        },
+
+        // plugins: [
+        //     new webpack.ProvidePlugin({
+        //         $               : 'jquery',
+        //         jQuery          : 'jquery',
+        //         'window.jQuery' : 'jquery',
+        //         // _map            : ['lodash', 'map'],
+        //     })
+        // ],
+    };
+};
